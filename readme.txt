00280a48f5a81e51a89a730ac75871a2829f6ddb
RyanJMah <rmah.uwaterloo.ca>
2020-10-16 04:15:51 -0600

    added lots of stuff

--- readme.txt
+++ readme.txt
@@ -1,3 +1,9 @@
 The purpose of this project is to try to make an automatic music transcriber.
 
 In progress.
+
+pydub requires a program called "ffmpeg" to function. To install it, run
+   
+    >> sudo apt-get install ffmpeg
+
+on ubuntu.
---
+++ src/audio_io.py
@@ -0,0 +1,32 @@
+import os
+import numpy as np
+from pydub import AudioSegment
+
+THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+
+def read_mp3(path):
+    sound = AudioSegment.from_mp3(path)
+
+    samples = list(np.frombuffer(sound.raw_data, dtype = np.int32))
+
+    T = 1/sound.frame_rate
+    times = [T*i for i in range(len(samples))]
+
+    return {"time": times, "amplitude": samples}
+
+
+def test():
+    import matplotlib.pyplot as plt
+    import csv
+    data = read_mp3("/mnt/c/shared/Personal/Projects/Auto-Transcription/samples/c_major_115_bpm.mp3")
+
+    with open("dump/samples.csv", "w") as f:
+        writer = csv.writer(f)
+        for t, f in zip(data["time"], data["amplitude"]):
+            writer.writerow([t, f])
+            
+
+
+if __name__ == "__main__":
+    test()
+
